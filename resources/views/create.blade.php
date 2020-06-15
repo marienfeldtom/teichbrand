636738cc11dc21ee36bb32ceb61c8e7d0358d135
Tom Marienfeld <marienfeldtom@googlemail.com>
2020-06-15 14:08:27 +0200

    add entwurf text

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -82,7 +82,7 @@
     </div>
 
     <div class="options md:flex md:space-x-6 text-sm items-center text-gray-700 mt-4">
-        <p class="w-1/2 mb-2 md:mb-0">T-Shirt? (15€) <button onclick="showTshirt()" type="button" class="btn btn-outline-primary"><svg class="bi bi-eye-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <p class="w-1/2 mb-2 md:mb-0">T-Shirt? (15€) <br><small>Shirt Entwurf:</small><button onclick="showTshirt()" type="button" class="btn btn-outline-primary"><svg class="bi bi-eye-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
                     <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
                 </svg></button></p>

936f31acd61ead4039c053d7d8abde2c2d37bf5b
Tom Marienfeld <marienfeldtom@googlemail.com>
2020-06-17 12:30:16 +0200

    change tshirt file

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -142,7 +142,7 @@
         Swal.fire({
             title: 'T<span class="text-xs" style="line-height: 4rem">eichbrand</span>-Shirt!',
             text: 'Zum richtigen Festivalerlebnis gehört auch das passende Outfit! (Quanta costa? 15 Euronen). T-Shirt fällt kleiner aus.',
-            imageUrl: '/tshirt.jpg',
+            imageUrl: '/tshirt3.jpg',
             imageWidth: 400,
             imageHeight: 200,
             imageAlt: 'T-Shirt',

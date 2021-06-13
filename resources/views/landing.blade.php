379697a0bebd2612387b5a0c9f4d0279161d5f4b
Tom Marienfeld <marienfeldtom@googlemail.com>
2021-06-13 15:53:19 +0200

    update logo

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -92,7 +92,7 @@
             </div>
             <!--Right Col-->
             <div class="w-full md:w-3/5 py-6 text-center">
-                <img class="w-full md:w-4/5 z-50 pb-20" src="logotb.png">
+                <img class="w-full md:w-4/5 z-50 pb-20" src="logo2021.png">
             </div>
 
         </div>
